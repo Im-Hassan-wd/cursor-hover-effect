4c69735773adc0b773a54ce6ec63cc24175bd52c
Im-Hassan-wd <alhassansahih@gmail.com>
2021-08-24 11:56:26 +0100

    initializing

---
+++ sandbox.js
@@ -0,0 +1,20 @@
+const mouseCursor = document.querySelector('.cursor');
+const links = document.querySelectorAll('nav li');
+
+window.addEventListener('mousemove', cursor);
+
+function cursor(e) {
+  // console.log(mouseCursor)
+  mouseCursor.style.top = e.pageY + 'px';
+  mouseCursor.style.left = e.pageX + 'px';
+}
+
+links.forEach(link => {
+  link.addEventListener('mouseover', () => {
+    mouseCursor.classList.add('link-grow');
+  });
+
+  link.addEventListener('mouseleave', () => {
+    mouseCursor.classList.remove('link-grow');
+  });
+});
